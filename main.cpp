6ff36ae27b5ef28e566f9e106311a162a60534a5
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-09-29 07:48:54 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void maximum(int* arr1, int len) {
+	int max = *arr1;
+	for (int a = 0; a < len; a++) {
+		if (max < *(arr1 + a))
+			max = *(arr1 + a);
+	}
+	cout << "Max: " << max << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int arr1[] = { 1, 2, -3, 4, 5, -6 };
+	maximum(arr1, 6);
+
+	return 0;
+}
